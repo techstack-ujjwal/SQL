7ec0917ea2bd1773add30730b474e342fb8b4083
UJJWAL SUMAN <ujjwalrajan2@gmail.com>
2026-06-19 13:04:24 +0530

    updated  my first table

--- 01-class.sql
+++ 01-class.sql
@@ -1,15 +1,20 @@
-CREATE TABLE students (
-    student_id SERIAL PRIMARY KEY, -- primary key is unique  and non null
-    first_name VARCHAR(50), NOT NULL   -- 50 letter can be added // or it will consuming 50 bytes
-    last_name VARCHAR(50),  50 * 8 bit
-    email VARCHAR(322), UNIQUE NOT NULL,
-    phone_number VARCHAR(10), UNIQUE, -- 4000 bytes vs 10 bytes
-    country_code VARCHAR(4),
-    age INT CHECK (age>18),
+-- CREATE TABLE students (
+--     student_id SERIAL PRIMARY KEY, -- primary key is unique  and non null
+--     first_name VARCHAR(50) NOT NULL ,  -- 50 letter can be added // or it will consuming 50 bytes
+--     last_name VARCHAR(50),-- 50 * 8 bit
+--     email VARCHAR(322) UNIQUE NOT NULL,
+--     phone_number VARCHAR(10) UNIQUE, -- 4000 bytes vs 10 bytes
+--     country_code VARCHAR(4),
+--     age INT CHECK (age>18),
 
-    current_status VARCHAR(20) DEFAULT 'active' CHECK (current_status IN ('active', 'gardaute', 'dropout')),
-    masterji_handle VARCHAR(50) BOOLEAN DEFAULT FALSE,
-    current_score INT DEFAULT 0 CHECK (current_score>= 0 AND current-score <=100),
-    enrollment_date DATETIME DEFAULT CURRENT_DATE, ---This is UTC
-)
+--     current_status VARCHAR(20) DEFAULT 'active' CHECK (current_status IN ('active', 'gradaute', 'dropout')),
+--     has_masterji_handle  BOOLEAN DEFAULT FALSE,
+--     current_score INT DEFAULT 0 CHECK (current_score>= 0 AND current_score <=100),
+--     enrollment_date DATE DEFAULT CURRENT_DATE  ---This is UTC
+-- );
 
+ALTER TABLE students
+ADD COLUMN batch_name VARCHAR(50) DEFAULT 'Web Dev 2026';
+
+
+--WHAT IS DDL?
